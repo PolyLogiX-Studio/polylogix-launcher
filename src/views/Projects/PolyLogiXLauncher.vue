1a543935bca600d8484f85523c211f210c34fe78
Ethan Snow <bombitmanbomb@gmail.com>
2020-08-18 01:18:53 -0400

    d

--- src/views/Projects/PolyLogiXLauncher.vue
+++ src/views/Projects/PolyLogiXLauncher.vue
@@ -14,7 +14,7 @@ export default {
   methods:{
     addDownload:function(){
       console.log("STATE")
-      this.$store.commit('addDownload')
+      this.$store.dispatch('addDownload')
     }
   }
 };
